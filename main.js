5091accf4045da51d9701d99f639a7fd527caa95
Crfil <strizhovhotmail@gmail.com>
2020-03-13 22:13:52 +0300

    Мой первый коммит

---
+++ main.js
@@ -0,0 +1,55 @@
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(second);
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log()
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone[`name`]);
+
+let arr = ['eminem.png', 'dr.dre.txt', 'madonna.bmp'];
+
+console.log(arr[2]);
+
+// alert('HEllo World');
+
+// let answer = confirm('Go home');
+
+// console.log(answer);
+
+// let answer = +prompt('Есть лит вам 18?', 'Да');
+
+// console.log(typeof(answer));
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+// console.log("2" === 2);
+
+let isCecked = false;
+    isClose = false;
+
+console.log(isCecked || isClose);
+
+
+
+
